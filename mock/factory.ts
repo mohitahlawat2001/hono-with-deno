ee1bab70da21ae74dd994bce28d198118912854b
Mohit ahlawat <65100859+mohitahlawat2001@users.noreply.github.com>
2024-12-22 13:55:25 +0000

    Add factories for generating mock animal and personal detail data

--- mock/factory.ts
+++ mock/factory.ts
@@ -1,5 +1,6 @@
 import { factory } from "@findhow/zod-factory";
 import AnimalSchema from "./animal.schema.ts";
+import PersonalDetailSchema from "./personal-details.schema.ts";
 
 const AnimalFactory = factory(AnimalSchema, (faker) => ({
   id: faker.string.uuid(),
@@ -7,4 +8,15 @@ const AnimalFactory = factory(AnimalSchema, (faker) => ({
   animal: faker.animal.cat(),
 }));
 
-export default AnimalFactory;
+const PersonalDetailFactory = factory(PersonalDetailSchema, (faker) => ({
+  id: faker.number.int(),
+  firstName: faker.person.firstName(),
+  lastName: faker.person.lastName(),
+  email: faker.internet.email(),
+  password: faker.internet.password(),
+}));
+
+const factoryAnimals = AnimalFactory.createMany(100);
+const factoryPeople = PersonalDetailFactory.createMany(100);
+
+export { AnimalFactory, factoryAnimals, factoryPeople, PersonalDetailFactory };
